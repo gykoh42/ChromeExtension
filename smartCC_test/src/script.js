4c742d351185ad85261f899fd63a1f1783e253b4
gykoh42 <karmakoh42@naver.com>
2024-06-24 16:10:07 +0900

    feat: TopNav 관련 onClick 함수 구현

---
+++ smartCC_test/src/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("options-btn").addEventListener("click", function () {
+    window.location.href = "options.html";
+  });
+
+  document.getElementById("home-btn").addEventListener("click", function () {
+    window.location.href = "home.html";
+  });
+});
